be48390d0113f184b9cd8bb55a6cd1a403204544
EDermendjiev <82662209+EDermendjiev@users.noreply.github.com>
2022-05-16 23:17:06 +0300

    Add files via upload

---
+++ StudentInfoSystem/StudentData.cs
@@ -0,0 +1,43 @@
+﻿using StudentInfoSystem.Enums;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentInfoSystem
+{
+    public class StudentData
+    {
+        public List<Student>? TestStudents { get; private set; }
+
+        public StudentData()
+        {
+            TestStudents = new List<Student>();
+
+            if (!IsThereStudent("121219000"))
+            {
+                //TestStudents?.Add(new Student()
+                //{
+                //    FirstName = "Ivan",
+                //    MiddleName = "Georgiev",
+                //    LastName = "Ivanov",
+                //    Speciality = "Computer and software engineering",
+                //    Faculty = "FCST",
+                //    Course = 3,
+                //    FacultyNumber = "121219000",
+                //    Group = 30,
+                //    Degree = Degree.Професионално,
+                //    Status = EducationStatus.Редовен,
+                //    Stream = 9
+                //});
+
+            }
+        }
+
+        public bool IsThereStudent(string facNum)
+        {
+            StudentInfoContext context = new StudentInfoContext();
+
+            Student? result = context.Students.SingleOrDefault(s => s.FacultyNumber == facNum);
+            return result != null;
+        }
+    }
+}
